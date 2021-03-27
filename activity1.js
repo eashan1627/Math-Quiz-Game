9799c054e84e71e7d823564617976cd088cc4f8b
eashan1627 <74692161+eashan1627@users.noreply.github.com>
2021-03-27 15:27:18 -0500

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+var score= 0;
+ 
+ function update_score() {
+ score= score + 1;
+ document.getElementById("score").innerHTML="Score: " + score;
+ }
+
+ function save_score() {
+    localStorage.setItem("score", score);
+ }
+
+ function next_page() {
+    window.location="activity2.html";
+ }
